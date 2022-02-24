bfe62f407288e212305a35d246d08196edf77913
Resaw <resavv@mail.ru>
2022-02-24 20:12:35 +0300

    webpack and pug setup

---
+++ webpack.config.js
@@ -0,0 +1,23 @@
+const path = require('path')
+const PugPlugin = require('pug-plugin')
+
+module.exports = {
+  output: {
+    path: path.join(__dirname, 'dist/'),
+    publicPath: '/', // must be defined any path, `auto` is not supported yet
+  },
+  entry: {
+    index: './src/index.pug', // the `pug-plugin` extract HTML from entry file
+  },
+  plugins: [
+    new PugPlugin(), // add it to handle pug files in entry
+  ],
+  module: {
+    rules: [
+      {
+        test: /\.pug$/,
+        use: PugPlugin.loader, // the pug-plugin already contain this pug-loader
+      },
+    ],
+  },
+};
